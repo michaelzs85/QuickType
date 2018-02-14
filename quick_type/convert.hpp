3c02c1aea2a35edd9d94d46bd3f50fe6fc8d24cf
Michael Zöhrer-Sauer <michaelzs@outlook.at>
2018-02-14 19:51:42 +0100

    put the conversion stuff in its own header file

---
+++ quick_type/convert.hpp
@@ -0,0 +1,44 @@
+#ifndef CONVERT_HPP
+#define CONVERT_HPP
+
+template <typename T1, typename T2, T2 (*from)(T1), T1 (*to)(T2)> struct convert
+{
+    static T2 convertFrom(T1 t)
+    {
+        return from(t);
+    }
+    static T1 convertTo(T2 t)
+    {
+        return to(t);
+    }
+};
+
+template <typename T1, typename T2, T2 (*f)(T1), T1 (*g)(T1)> struct compose_from
+{
+    static T2 func(T1 t)
+    {
+        return f(g(t));
+    }
+};
+
+template <typename T1, typename T2, T1 (*f)(T1), T1 (*g)(T2)> struct compose_to
+{
+    static T1 func(T2 t)
+    {
+        return f(g(t));
+    }
+};
+
+template <typename T1, typename T2, typename Ratio> struct convert_with_ratio
+{
+    static T1 convertFrom(T2 t)
+    {
+        return static_cast<T1>(t) * Ratio::den / Ratio::num;
+    }
+    static T2 convertTo(T1 t)
+    {
+        return static_cast<T2>(t * Ratio::num / Ratio::den);
+    }
+};
+
+#endif // CONVERT_HPP
--- quick_type/quick_type.hpp
+++ quick_type/quick_type.hpp
@@ -1,52 +1,13 @@
 #ifndef NAMEDTYPE_H
 #define NAMEDTYPE_H
 
+#include "convert.hpp"
 #include "operators.hpp"
 #include "operators_witharithmetictypes.hpp"
 #include "operators_withconvert.hpp"
 #include "skills.hpp"
 #include <ratio>
 #include <type_traits>
-
-template <typename T1, typename T2, T2 (*from)(T1), T1 (*to)(T2)> struct convert
-{
-    static T2 convertFrom(T1 t)
-    {
-        return from(t);
-    }
-    static T1 convertTo(T2 t)
-    {
-        return to(t);
-    }
-};
-
-template <typename T1, typename T2, T2 (*f)(T1), T1 (*g)(T1)> struct compose_from
-{
-    static T2 func(T1 t)
-    {
-        return f(g(t));
-    }
-};
-
-template <typename T1, typename T2, T1 (*f)(T1), T1 (*g)(T2)> struct compose_to
-{
-    static T1 func(T2 t)
-    {
-        return f(g(t));
-    }
-};
-
-template <typename T1, typename T2, typename Ratio> struct convert_with_ratio
-{
-    static T1 convertFrom(T2 t)
-    {
-        return static_cast<T1>(t) * Ratio::den / Ratio::num;
-    }
-    static T2 convertTo(T1 t)
-    {
-        return static_cast<T2>(t * Ratio::num / Ratio::den);
-    }
-};
 
 template <typename T, class Tag, class Converter, class... Skills> struct quick_type_impl : public Skills..., Converter
 {
@@ -83,7 +44,6 @@ using multiple_of2 = typename quick_type::conversions::template GetConvertible<T
 template <class quick_type, class Ratio> using multiple_of = multiple_of2<quick_type, decltype(quick_type::val), Ratio>;
 
 // hashing
-
 namespace std
 {
 
